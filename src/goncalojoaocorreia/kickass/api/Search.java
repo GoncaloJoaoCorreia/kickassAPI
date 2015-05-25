90d7f9f4253ede0e8bcb4ee1180c7a2874141289
GoncaloJoaoCorreia <goncalojoaocorreia@gmail.com>
2015-05-25 10:39:33 +0100

    Search.next() and Search.page() methods implemented

--- src/goncalojoaocorreia/kickass/api/Search.java
+++ src/goncalojoaocorreia/kickass/api/Search.java
@@ -32,6 +32,10 @@ public class Search extends ArrayList<Torrent> {
 
 	private static int timeout = 1000;
 
+	private String searchURL;
+	private int currentPage;
+	private final int pageCount;
+
 	public enum SortOption {
 
 		SMALLEST_FIRST, LARGEST_FIRST, OLDEST_FIRST, YOUNGEST_FIRST, MOST_SEEDERS, LEAST_SEEDERS, MOST_LEECHES, LEAST_LEECHES
@@ -39,8 +43,102 @@ public class Search extends ArrayList<Torrent> {
 
 	//TODO: .next() method to iterate through pages
 	//Store search url in object when doing newSearch()
+	public Search next() throws IOException {
+		if (currentPage < pageCount) {
+			this.currentPage++;
+			this.formatPageURL();
+			return Search.runSearch(new URL(this.searchURL));
+		} else {
+			return this;
+		}
+	}
+
+	public Search page(int pageNumber) throws IOException {
+		if (currentPage < pageCount && pageNumber > 0) {
+			this.currentPage = pageNumber;
+			this.formatPageURL();
+			return Search.runSearch(new URL(this.searchURL));
+		} else {
+			return this;
+		}
+	}
+
+	public static Search newSearch(String query) throws IOException {
+		String urlStr = SEARCH_URL + URLEncoder.encode(query, "UTF-8");
+		URL url = new URL(urlStr);
+
+		return Search.runSearch(url);
+	}
+
+	public static Search newSearch(String query, Category category) throws IOException {
+		String urlStr = SEARCH_URL + URLEncoder.
+			encode(query + CATEGORY_SEARCH + category.url(), "UTF-8");
+		URL url = new URL(urlStr);
+
+		return Search.runSearch(url);
+	}
+
+	public static Search newSearch(String query, SortOption sort) throws IOException {
+		String urlStr = SEARCH_URL + URLEncoder.
+			encode(query + getSortQuery(sort), "UTF-8");
+		URL url = new URL(urlStr);
+		return Search.runSearch(url);
+	}
+
+	public static Search newSearch(String query, Category category,
+								   SortOption sort) throws IOException {
+		String urlStr = SEARCH_URL + URLEncoder.
+			encode(query + CATEGORY_SEARCH + category.url() + getSortQuery(sort), "UTF-8");
+		URL url = new URL(urlStr);
+		return Search.runSearch(url);
+	}
+
+	/**
+	 * Sets the timeout used by the parsing methods. Default value is 1000.
+	 *
+	 * @param t Time in milliseconds
+	 */
+	public static void setTimeout(int t) {
+		timeout = t;
+	}
+
 	private Search() {
 		super();
+		searchURL = "";
+		currentPage = 0;
+		pageCount = 0;
+	}
+
+	private Search(String searchURL, int pageCount) {
+		super();
+		this.searchURL = searchURL;
+		this.currentPage = 1;
+		this.pageCount = pageCount;
+	}
+
+	private String formatPageURL() {
+		String[] spl = this.searchURL.split("/");
+		if (this.searchURL.contains("?")) {
+			if (spl.length == 7) {
+				spl[spl.length - 2] = "" + this.currentPage;
+			} else if (spl.length == 6) {
+				spl[spl.length - 1] = this.currentPage + "/" + spl[spl.length - 1];
+			}
+		} else {
+			if (spl.length == 6) {
+				spl[spl.length - 1] = "" + this.currentPage;
+			} else if (spl.length == 5) {
+				this.searchURL += this.currentPage;
+				return this.searchURL;
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (String s : spl) {
+			sb.append(s);
+			sb.append("/");
+		}
+		this.searchURL = sb.toString();
+		return this.searchURL;
 	}
 
 	private static Search runSearch(URL url) throws IOException {
@@ -51,7 +149,10 @@ public class Search extends ArrayList<Torrent> {
 			//404 error means no torrents found in search, so we return an empty list
 			return new Search();
 		}
-		Search search = new Search();
+		Elements pages = doc.select("a.turnoverButton.siteButton.bigButton");
+		int maxPages = Integer.parseInt(pages.get(pages.size() - 1).text());
+
+		Search search = new Search(url.toString(), maxPages);
 
 		Elements torrents = doc.select("tr").not(".firstr");
 
@@ -107,45 +208,6 @@ public class Search extends ArrayList<Torrent> {
 				break;
 		}
 		return sortQuery;
-	}
-
-	public static Search newSearch(String query) throws IOException {
-		String urlStr = SEARCH_URL + URLEncoder.encode(query, "UTF-8");
-		URL url = new URL(urlStr);
-
-		return Search.runSearch(url);
-	}
-
-	public static Search newSearch(String query, Category category) throws IOException {
-		String urlStr = SEARCH_URL + URLEncoder.
-			encode(query + CATEGORY_SEARCH + category.url(), "UTF-8");
-		URL url = new URL(urlStr);
-
-		return Search.runSearch(url);
-	}
-
-	public static Search newSearch(String query, SortOption sort) throws IOException {
-		String urlStr = SEARCH_URL + URLEncoder.
-			encode(query + getSortQuery(sort), "UTF-8");
-		URL url = new URL(urlStr);
-		return Search.runSearch(url);
-	}
-
-	public static Search newSearch(String query, Category category,
-								   SortOption sort) throws IOException {
-		String urlStr = SEARCH_URL + URLEncoder.
-			encode(query + CATEGORY_SEARCH + category.url() + getSortQuery(sort), "UTF-8");
-		URL url = new URL(urlStr);
-		return Search.runSearch(url);
-	}
-
-	/**
-	 * Sets the timeout used by the parsing methods. Default value is 1000.
-	 *
-	 * @param t Time in milliseconds
-	 */
-	public static void setTimeout(int t) {
-		timeout = t;
 	}
 
 }
